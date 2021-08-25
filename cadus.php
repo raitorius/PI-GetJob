123d5194e7e35fee7fdc5b2d426bd3a68093631c
Kael Franca <kael.franca@alunosc.sc.senac.br>
2021-08-25 17:04:23 -0300

    alteraçoes no include-once linha 25

--- cadus.php
+++ cadus.php
@@ -1,5 +1,5 @@
 <?php
-// include_once(dirname(__FILE__)."/../layout/Mysql.php");
+// include_once(dirname(__FILE__)."./MySQL.php");
 
 
 
@@ -22,23 +22,11 @@
 //     }
 // }
 
-// include_once("header.php");
+ include_once("./inc/header.php");
 ?>
 
 
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CadFuncionário</title>
-    <link rel="stylesheet" href="fuction/logStyle.css">
-  
-</head>
-
-<body>
     <main class="container">
     <form action="" method="POST"  accept-charset="UTF-8">
             <fieldset>

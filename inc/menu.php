d6f2e37396b25bdb9298c307fc63e4c3afda587c
Lean12 <81381897+Lean12@users.noreply.github.com>
2021-09-09 16:02:30 -0300

    Update menu.php

--- inc/menu.php
+++ inc/menu.php
@@ -12,7 +12,7 @@
           <div class="bg-dark p-5">
             <h4 class="text-white">MENU</h4>
             <ul class="col my-5">
-              <li class="my-3"><a href="index.php"><img class="w-25 h-25" src="assets/img/house.png">Inicial</a></li>
+              <li class="my-3"><a href="index.php">Inicial</a></li>
 
               <div class="btn-group dropright">
 
@@ -29,27 +29,30 @@
               <div class="btn-group dropright">
 
 
-                <li class="my-3type  " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href="login.php">Login</a></li>
+                <li class="my-3type=  " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href="login.php">Login</a></li>
                 <div class="dropdown-menu">
                   <ul>
                     <li> <a href="loginfuncionario.php">Funcionário</a></li>
-                    <li><a href="login.php">Usuário</a></li>
+                
                   </ul>
                 </div>
               </div>
 
               <div class="btn-group dropright">
 
-                <li class="my-3type  " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href=" cadfuncionario.php">Serviços </a> </li>
+                <li class="my-3type=  " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href=" cadfuncionario.php">Serviços </a> </li>
                 <div class="dropdown-menu">
                   <ul>
                     <li> <a href="servicos.php">Funcionários</a></li>
 
-                   
-
+                    <div class="dropdown-menu">
+                      <ul>
+                        <li> <a href="cadfuncionario.php">Funcionário</a></li>
+                        <li><a href="cadusuario.php">Usuário</a></li>
+                      </ul>
+                    </div>
                 </div>
 
-              
 
 
             </ul>

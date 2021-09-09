22311e71ccf3de564a1ebc61612b1bc5cc309c1f
Gustavo Reinert <senacreinert@gmail.com>
2021-09-09 14:56:47 -0300

    exercicio Kaelk

--- cadfuncionario.php
+++ cadfuncionario.php
@@ -1,11 +1,22 @@
 <?php
- 
-include_once(dirname(__FILE__)."/inc/MySQL.php");
+
+include_once(dirname(__FILE__) . "/inc/MySQL.php");
 
 
- 
+
 if (isset($_POST['cadastro'])) {
-    $img = $_POST['img'];
+    
+    
+    // Pegar a imagem postada e inserir no banco
+    // por campo BLOB
+    /**
+     * Estudar file_get_contents() $_FILES
+     * 
+     */
+    $img = $_FILES['img'];
+    
+    
+    
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $formacao = $_POST['formacao'];
@@ -13,11 +24,14 @@ if (isset($_POST['cadastro'])) {
     $telefone = $_POST['telefone'];
     $descricao = $_POST['descricao'];
     $senha = md5($_POST['senha']);
-      //acentuação da erro 
+
+
+    
+    //acentuação da erro 
     $sql = $pdo->prepare("INSERT INTO funcionario (id,nome,email,formacao,profissao,telefone,descricao,senha) values (null,?,?,?,?,?,?,?)");
     if ($sql->execute(array($nome, $email, $formacao, $profissao, $telefone, $descricao, $senha))) {
         echo 'Dados cadastrados com sucesso.';
-       //header, faz o redcionamento das páginas//
+        //header, faz o redcionamento das páginas//
         header('location:/GetJob-ProjetoIntegrador/loginfuncionario.php');
     } else {
         echo 'Dados não cadastrados!';
@@ -27,70 +41,70 @@ if (isset($_POST['cadastro'])) {
 
 <?php
 
-include_once(dirname(__FILE__)."/inc/header.php");
+include_once(dirname(__FILE__) . "/inc/header.php");
 
-include_once(dirname(__FILE__)."/inc/menu.php");
+include_once(dirname(__FILE__) . "/inc/menu.php");
 
 
 ?>
 
 
-    <main class="container">
-      <form action="" method="POST">
-            <fieldset>
-                <h1>Cadastro de Funcionário</h1>
-                <br>
-                <p>Dados Pessoais</p>
-                <div class="input-field">
-                    <input type="text" name="nome" id="nome" placeholder="Nome Completo" required>
-                    <div class="efeito"></div>
-                </div>
-                <br>
-                <div class="input-field">
-                    <input type="email" name="email" id="email" placeholder="Email" required>
-                    <div class="efeito"></div>
-                </div>
-                <br>
-                <div class="input-field">
-                    <input type="text" name="telefone" id="telefone" maxlength="10" required placeholder="Telefone">
-                    <div class="efeito"></div>
-                </div>
-                <br>
-                <div>
-                   <h6 class="font2">Formação</h6>
-                    <input type="radio" name="formacao" id="formacao" value="fundamental" required>Fundamental
-                    <input type="radio" name="formacao" id="formacao" value="médio" required>Ensino-Médio
-                    <input type="radio" name="formacao" id="formacao" value="superior" required>Ensino-Superior
-                </div>
-                <br>
-                <div>
-                    <textarea name="profissao" id="profissao" cols="45" rows="3" placeholder="Profissões" maxlength="500"></textarea>
-                </div>
-                <br>
-                <div>
-                    <textarea name="experiencia" id="experiencia" cols="45" rows="5" placeholder="Experiência" maxlength="500"></textarea>
-                </div>
-                <br>
-                <div>
-                    <textarea name="descricao" id="descricao" cols="45" rows="8" placeholder="Descrição" maxlength="500"></textarea>
-                </div>
-                <div>
-                <label for="img">Imagem de Perfil</label>
-            <input  type="file" name="img" id="img" />
+<main class="container">
+    <form action="" method="POST" enctype='multipart/form-data'>
+        <fieldset>
+            <h1>Cadastro de Funcionário</h1>
             <br>
-            <p>Clique para selecionar uma imagem de perfil*</p>
+            <p>Dados Pessoais</p>
+            <div class="input-field">
+                <input type="text" name="nome" id="nome" placeholder="Nome Completo" required>
+                <div class="efeito"></div>
             </div>
+            <br>
+            <div class="input-field">
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <div class="efeito"></div>
+            </div>
+            <br>
+            <div class="input-field">
+                <input type="text" name="telefone" id="telefone" maxlength="10" required placeholder="Telefone">
+                <div class="efeito"></div>
+            </div>
+            <br>
+            <div>
+                <h6 class="font2">Formação</h6>
+                <input type="radio" name="formacao" id="formacao" value="fundamental" required>Fundamental
+                <input type="radio" name="formacao" id="formacao" value="médio" required>Ensino-Médio
+                <input type="radio" name="formacao" id="formacao" value="superior" required>Ensino-Superior
+            </div>
+            <br>
+            <div>
+                <textarea name="profissao" id="profissao" cols="45" rows="3" placeholder="Profissões" maxlength="500"></textarea>
+            </div>
+            <br>
+            <div>
+                <textarea name="experiencia" id="experiencia" cols="45" rows="5" placeholder="Experiência" maxlength="500"></textarea>
+            </div>
+            <br>
+            <div>
+                <textarea name="descricao" id="descricao" cols="45" rows="8" placeholder="Descrição" maxlength="500"></textarea>
+            </div>
+            <div>
+                <label for="img">Imagem de Perfil</label>
+                <input type="file" name="img" id="img" />
                 <br>
-                <br>
-                <div class="input-field">
-                    <input type="password" name="senha" id="senha" placeholder="Senha" minlength="5" maxlength="15">
-                    <div class="efeito"></div>
-                </div>
+                <p>Clique para selecionar uma imagem de perfil*</p>
+            </div>
+            <br>
+            <br>
+            <div class="input-field">
+                <input type="password" name="senha" id="senha" placeholder="Senha" minlength="5" maxlength="15">
+                <div class="efeito"></div>
+            </div>
 
-                <input type="submit" name="cadastro" value="Cadastrar Funcionário">
-            </fieldset>
-        </form>
-    </main>
- <?php
-include_once(dirname(__FILE__)."/inc/footer.php");
+            <input type="submit" name="cadastro" value="Cadastrar Funcionário">
+        </fieldset>
+    </form>
+</main>
+<?php
+include_once(dirname(__FILE__) . "/inc/footer.php");
 ?>
--- servicos.php
+++ servicos.php
@@ -1,29 +1,29 @@
 <?php
-include_once(dirname(__FILE__)."/inc/MySQL.php");
+include_once(dirname(__FILE__) . "/inc/MySQL.php");
 
-include_once(dirname(__FILE__)."./inc/header.php");
+include_once(dirname(__FILE__) . "./inc/header.php");
 
 ?>
 <?php
 
-include_once(dirname(__FILE__)."./inc/menu.php");
+include_once(dirname(__FILE__) . "./inc/menu.php");
 
 ?>
 
 <h1>Serviços cadastrados no GetJob:</h1>
 
 <style>
-    body{
+  body {
     background: white;
-     border-radius: 4px;
-    
-    }
+    border-radius: 4px;
 
-  .test{
-      border-radius: 6px;
-      background: linear-gradient(45deg, #ffffff, #C0C0C0, rgb(220,220,220));
-      font-family: 'Poppins', sans-serif;
-      box-shadow: 2px 2px 8px black;
+  }
+
+  .test {
+    border-radius: 6px;
+    background: linear-gradient(45deg, #ffffff, #C0C0C0, rgb(220, 220, 220));
+    font-family: 'Poppins', sans-serif;
+    box-shadow: 2px 2px 8px black;
     border: solid;
     border-color: black;
     border-radius: 6px;
@@ -31,56 +31,55 @@ include_once(dirname(__FILE__)."./inc/menu.php");
 
   }
 
-  .img{
-   
-  }
+  .img {}
 </style>
-          
+
 <div class="regis">
 
 
-    <?php
-    $sql = $pdo->prepare('SELECT * FROM funcionario order by profissao');
-    if ($sql->execute()) {
-        $info = $sql->fetchAll(PDO::FETCH_ASSOC);
+  <?php
+  $sql = $pdo->prepare('SELECT * FROM funcionario order by profissao');
+  if ($sql->execute()) {
+    $info = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($info as $key => $values) {
-            echo '<div class="test">';
-            echo '<div class="img">img:</div>  ' . $values['img'] . '';
-            echo 'nome: ' . $values['nome'] . '<br>';
-            echo 'email: ' . $values['email'] . '<br>';
-            echo 'formacao: ' . $values['formacao'] . '<br>';
-            echo 'profissao: ' . $values['profissao'] . '<br>';
-            echo 'telefone: ' . $values['telefone'] . '<br>';
-            echo 'descricao:  ' . $values['descricao'] . '<br>';
-            echo "<a href='user.php?id=" . $values['id'] . "'>(Ver Perfil)</a>";
-            echo "</div>";
-        
+    foreach ($info as $key => $values) {
+      echo '<div class="row test">';
+      echo '<div class=" col-4">';
+      echo '<div class="img">img:</div>  ' . $values['img'] . '';
+      echo "</div>";
+      echo '<div class="text-left col-8 p-3">';
 
-        
-            
+      echo 'nome: ' . $values['nome'] . '<br>';
+      echo 'email: ' . $values['email'] . '<br>';
+      echo 'formacao: ' . $values['formacao'] . '<br>';
+      echo 'profissao: ' . $values['profissao'] . '<br>';
+      echo 'telefone: ' . $values['telefone'] . '<br>';
+      echo 'descricao:  ' . $values['descricao'] . '<br>';
+      echo "<a href='user.php?id=" . $values['id'] . "'>(Ver Perfil)</a>";
+      echo "</div>";
+      echo "</div>";
 
 
 
 
-            echo '<hr>';
-        }
+
+
+
+
+      echo '<hr>';
     }
+  }
 
 
-    //echo ("Olá O usuário:$ é o usuário número $id. sua descricao é: $descricao");
+  //echo ("Olá O usuário:$ é o usuário número $id. sua descricao é: $descricao");
 
-    //echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br> Formação: $formacao<br>Telefone: $telefone<br>Descrição: $descricao<Br>Senha:$senha</a>");
+  //echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br> Formação: $formacao<br>Telefone: $telefone<br>Descrição: $descricao<Br>Senha:$senha</a>");
 
-    ?>
+  ?>
 </div>
 
 <?php
 
-include_once(dirname(__FILE__)."./inc/footer.php");
+include_once(dirname(__FILE__) . "./inc/footer.php");
 
 ?>
-
-
-
-

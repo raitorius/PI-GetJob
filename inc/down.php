b972a0b240cd6b264e4a7289e28e5cbcf93ac55d
leandro.rosa@alunos.sc.senac.br <leandro.rosa@alunos.sc.senac.br>
2021-09-10 14:30:16 -0300

    att lele

--- inc/down.php
+++ inc/down.php
@@ -1,15 +1,12 @@
-<section class="">
-    <div class="container-fluid" style="background-color: gray" id="sobre">
-        <div class="row m-4 ">
-            <div class="col">
-            </div>
-            <div class="col">
-                <p style="color: black">Pesquisa rápida</p>
-                <a style="text-decoration: none; color: white" href="sobre.php">Sobre nós</a><br>
-            </div>
-        </div>
-        <div class="row">
-            <p style="color: black"; class="mx-end">Contato: GetJobRH@gmail.com</p>
-        </div>
-    </div>
-</section>
+  <div class="container-fluid" style="background-color: gray" id="sobre">
+      <div class="row">
+          <div class="col"></div>
+          <div class="col">
+              <p style="color: black">Pesquisa rápida</p>
+              <a style="text-decoration: none; color: white" href="sobre.php">Sobre nós</a><br>
+          </div>
+      </div>
+      <div class="row">
+          <p style="color: black" ; class="mx-end">Contato: GetJobRH@gmail.com</p>
+      </div>
+  </div>

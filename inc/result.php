18f44aba4fd9de0b47df029b1d71a37608ec17f7
leandro.rosa@alunos.sc.senac.br <leandro.rosa@alunos.sc.senac.br>
2021-09-13 15:03:45 -0300

    att lele

--- inc/result.php
+++ inc/result.php
@@ -12,7 +12,7 @@ $result_anun = "SELECT * FROM anuncios WHERE ID LIKE '$pesquisar' LIMIT 5";
 $result_anuncios = mysqli_query($teste, $result_anun);
 
 while ($rows_anun = mysqli_fetch_array($result_anuncios)) {
-    echo "Nome do anunci: " . $rows_anun['ne'] . "<br>";
+    echo "Nome do anunci: " . $rows_anun['nome'] . "<br>";
     echo "Descrição: " . $rows_anun['descricao'] . "<br>";
     echo "ID: " . $rows_anun['ID'] . "<br>";
     echo "<a href='workperf.php?id=" . $rows_anun['ID'] . "'>Acessar</a>";

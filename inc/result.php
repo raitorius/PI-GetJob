1eaa21997fe1e77f4005a710a3afebf1dd7841ab
leandro.rosa@alunos.sc.senac.br <leandro.rosa@alunos.sc.senac.br>
2021-09-13 14:47:33 -0300

    att lele

--- inc/result.php
+++ inc/result.php
@@ -1,22 +1,21 @@
 <?php
- $servidor = "localhost:3308";
- $usuario = "root";
- $senha = "";
- $dbname = "getjob";
- //Criar a conexao
+$servidor = "localhost:3308";
+$usuario = "root";
+$senha = "";
+$dbname = "getjob";
+//Criar a conexao
 
- $teste = mysqli_connect($servidor, $usuario, $senha, $dbname);
+$teste = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
 $pesquisar = $_POST['pesquisar'];
-$result_anun = "SELECT * FROM anuncios WHERE nome LIKE '$pesquisar' LIMIT 5";
+$result_anun = "SELECT * FROM anuncios WHERE ID LIKE '$pesquisar' LIMIT 5";
 $result_anuncios = mysqli_query($teste, $result_anun);
 
-while($rows_anun = mysqli_fetch_array($result_anuncios)){
-    echo "Nome do anuncio: ".$rows_anun['nome']."<br>";
-    echo "Descrição: " .$rows_anun['descricao']."<br>";
-    echo "<a href='workperf.php'>Acessar</a>";
+while ($rows_anun = mysqli_fetch_array($result_anuncios)) {
+    echo "Nome do anunci: " . $rows_anun['ne'] . "<br>";
+    echo "Descrição: " . $rows_anun['descricao'] . "<br>";
+    echo "ID: " . $rows_anun['ID'] . "<br>";
+    echo "<a href='workperf.php?id=" . $rows_anun['ID'] . "'>Acessar</a>";
     echo "<hr>";
+
 }
-
-
-?>
